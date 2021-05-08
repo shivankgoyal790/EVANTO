91cabb3aff942d6bb2123c7e7ec550c6e4261d4f
shivank goyal <goyal.shivank790@gmail.com>
2021-05-08 19:54:40 +0530

    cards added

---
+++ src/Evanto/CardContainer.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Card-container.css"
+import Card from "./Cards"
+const CardContainer = () =>{
+    return(
+            <div className="container1">
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+                <div className="flex-box1"><Card /></div>
+            </div>
+
+    );
+}
+
+export default CardContainer;
---
+++ src/Evanto/Cards.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Cards.css"
+
+const Card = () =>{
+
+    return(
+        <>
+            <div className="card-container">
+                    <h3>18+ Pages</h3>
+                    <p>This Theme comes with necessary features for your online presence like portfolio, blog
+                    page etc.</p>
+
+            </div>
+
+        </>
+    );
+}
+
+export default Card
