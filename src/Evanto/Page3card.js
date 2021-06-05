890fe6b03526734ce98e336f504b6815558906e8
shivank goyal <goyal.shivank790@gmail.com>
2021-06-05 20:26:07 +0530

    page3cards

---
+++ src/Evanto/Page3card.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Page3card.css"
+
+
+const Page3card = (props) =>{
+    return(
+            
+            <div className="page-container2">
+                <div className="imageover"><img src={props.imag} alt="bye" />
+                <div className="over"></div>
+                </div>
+                <h1>{props.head1}</h1>
+                <h3>Portfolio Home</h3>
+            </div>
+           
+
+   
+    )
+}
+
+export default Page3card
