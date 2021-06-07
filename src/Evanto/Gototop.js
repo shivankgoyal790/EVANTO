bc4e9fd0341aace6b4eccba0671ad9d1583b904d
shivank goyal <goyal.shivank790@gmail.com>
2021-06-07 19:20:49 +0530

    go to top component

---
+++ src/Evanto/Gototop.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import "./Gototop.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faAngleDoubleUp}  from "@fortawesome/free-solid-svg-icons"
+const Gototop = () =>{
+
+    const myfunction1 = () =>{
+        document.documentElement.scrollTop = 0;
+    }
+
+    const [visible,setvisible] = useState(false);
+    const toggleVisible = () =>{
+        if (document.documentElement.scrollTop > 10) {
+            setvisible(true);}
+        else {
+        setvisible(false);}
+    }
+    window.addEventListener('scroll', toggleVisible);
+
+    return(
+    <div className="scrolltotop">
+                <span className="gotobtn" style={{display : visible ? "block" : "none"}} onClick={myfunction1}>
+                <FontAwesomeIcon icon={faAngleDoubleUp} /></span>
+    </div>
+)
+}
+
+export default Gototop
