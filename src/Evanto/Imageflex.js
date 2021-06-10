eaa73c501799c44ef6ebfe2a3ec70cb08dfd01e7
shivank goyal <goyal.shivank790@gmail.com>
2021-06-10 23:30:29 +0530

    responsivess error fixed

--- src/Evanto/Imageflex.js
+++ src/Evanto/Imageflex.js
@@ -1,18 +1,16 @@
  import React from "react";
  import "./Imageflex.css";
+ import Logo1 from "./images/page-shape.jpg"
  const Imageflex = (props) =>{
 
     return(
         <>
         <div className="contain">
-            <div className="header">
-                <span className="cir"></span>
-                <span className="cir"></span>
-                <span className="cir"></span>
-                <div className="rec"></div>
-            </div>
+            <img src={Logo1} alt="hi" className="imagheader"/>
+            <div style={{position:"relative"}}>
             <img src={props.img} alt="hi" className="demo1"/>
             <p className="caption">{props.caption}</p>
+            </div>
         </div>
        
         </>
